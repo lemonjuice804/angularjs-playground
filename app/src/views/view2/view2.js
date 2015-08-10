825eeb7dc9e953ca3d69854baba224e6656ec848
Katherine <kcheng@riverbed.com>
2015-08-10 15:46:43 -0700

    Modify view 2 to support itune search using factory.

--- app/src/views/view2/view2.js
+++ app/src/views/view2/view2.js
@@ -9,6 +9,59 @@ angular.module('myApp.view2', ['ngRoute', 'myApp.config'])
   });
 }])
 
-.controller('View2Ctrl', [function() {
+.factory('View2Factory', function($http, $q) {
+    var service = {};
+    var baseUrl = 'https://itunes.apple.com/search?term=';
+    var _artist = 'tvxq';
+    var _finalUrl = '';
 
+    var makeUrl = function() {
+        _artist = _artist.split(' ').join('+');
+        _finalUrl = baseUrl + _artist + '&callback=JSON_CALLBACK';
+        return _finalUrl;
+    };
+
+    service.setArtist = function(artist) {
+        _artist = artist;
+    };
+
+    service.getArtist = function() {
+        return _artist;
+    };
+
+    service.callItunes = function() {
+        makeUrl();
+        var deferred = $q.defer();
+        $http({
+            method: 'JSONP',
+            url: _finalUrl
+        }).success(function(data) {
+            deferred.resolve(data);
+        }).error(function(err) {
+            console.error(err);
+            deferred.reject('There was an error.');
+        });
+        return deferred.promise;
+    };
+
+    return service;
+})
+
+.controller('View2Ctrl', ['$scope', 'View2Factory', function($scope, myFactory) {
+    $scope.data = {};
+
+    $scope.updateArtist = function() {
+        myFactory.setArtist($scope.data.artist);
+    };
+
+    $scope.submitArtist = function() {
+        this.updateArtist();
+
+        myFactory.callItunes()
+            .then(function(data){
+                $scope.data.artistData = data.results;
+            }, function(data){
+                console.log(data);
+            });
+    };
 }]);
